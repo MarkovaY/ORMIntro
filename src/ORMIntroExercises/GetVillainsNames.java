cd99177d604a8a6bd0c3929cd4fb1c2066532f2c
YanaMarkova <yana_markova@yahoo.com>
2023-06-26 16:20:18 +0200

    Initial commit

---
+++ src/ORMIntroExercises/GetVillainsNames.java
@@ -0,0 +1,35 @@
+package ORMIntroExercises;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GetVillainsNames {
+    private static final String GET_VILLAINS_NAMES = "SELECT v.name, COUNT(DISTINCT mv.minion_id) AS count_minions FROM villains v JOIN minions_villains mv ON v.id = mv.villain_id GROUP BY v.id HAVING count_minions > ? ORDER BY count_minions DESC";
+
+    private static final String STRING_FORMAT = "%s %d";
+
+    public static void main(String[] args) throws SQLException {
+
+        final Connection connection = Utils.getSQLConnection();
+
+        final PreparedStatement statement = connection.prepareStatement(GET_VILLAINS_NAMES);
+
+        statement.setInt(1, 15);
+
+        final ResultSet result = statement.executeQuery();
+
+        while (result.next()) {
+            print(result);
+        }
+
+        connection.close();
+    }
+
+    private static void print(ResultSet result) throws SQLException {
+        String name = result.getString("v.name");
+        int count = result.getInt("count_minions");
+        System.out.printf(STRING_FORMAT, name, count);
+    }
+}
